102b5e13fe5258559a2438cbdd1ff951342c1e51
jkachmar <git@jkachmar.com>
2023-05-02 11:32:23 -0400

    auto-start GPG's SSH agent

--- config/user/gpg/macos.nix
+++ config/user/gpg/macos.nix
@@ -6,7 +6,10 @@
 }: {
   imports = [./common.nix];
 
+  # The 'gpg-agent' module for 'home-manager' takes care of this, but it's
+  # Linux-only.
   programs.fish.shellInit = lib.optionalString config.programs.fish.enable ''
     set -x GPG_TTY (tty)
+    set -x SSH_AUTH_SOCK (${pkgs.gnupg}/bin/gpgconf --list-dirs agent-ssh-socket)
   '';
 }
