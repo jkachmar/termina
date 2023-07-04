13277aa9a66f04177dd84c4fc45f99f28c00554e
jkachmar <git@jkachmar.com>
2023-07-04 00:24:37 -0400

    chore: use systemd-resolved for tatl

--- hosts/tatl/networking.nix
+++ hosts/tatl/networking.nix
@@ -1,5 +1,14 @@
 {...}: {
+  services.resolved = {
+    enable = true;
+    fallbackDns = [ "9.9.9.9" "8.8.8.8" ];
+  };
+
   networking = {
+    # FIXME: `resolved` tries to get its DNS servers from the presently
+    # configured network manager; until i set up `networkd` one basically
+    # doesn't exist on a headless server such as this.
+    nameservers = [ "192.168.1.150" ];
     useDHCP = false;
     interfaces = {
       enp86s0.useDHCP = true;
