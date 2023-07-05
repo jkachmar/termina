fb59b55e54575cfdb00aef956d52b540e8948de0
jkachmar <git@jkachmar.com>
2023-07-04 20:52:37 -0400

    fix: disable systemd-resolved stub

--- hosts/tatl/networking.nix
+++ hosts/tatl/networking.nix
@@ -1,8 +1,18 @@
-{...}: {
+{lib, ...}: {
   services.resolved = {
     enable = true;
     fallbackDns = [ "9.9.9.9" "8.8.8.8" ];
+    # NOTE: [stub-resolver]
+    extraConfig = ''
+      DNSStubListener=no
+    '';
   };
+  # NOTE: [stub-resolver]
+  #
+  # DNS is being handled by a pretty miserable combination of PiHole &
+  # dnscrypt-proxy; let's not add the `systemd-resolved` stub resolver to it.
+  environment.etc."resolv.conf".source =
+    lib.mkForce  "/run/systemd/resolve/resolv.conf";
 
   networking = {
     dhcpcd.enable = false;
