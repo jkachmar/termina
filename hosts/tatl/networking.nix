2763d1f0b580b95ca42753a667a306c5df22099c
jkachmar <git@jkachmar.com>
2023-07-05 00:27:24 -0400

    Revert "feat: use systemd-networkd over networking.interfaces"
    This reverts commit 4a8ca8fce1e3f20cfc79e3720a4bf977a33e1d0e.
    
    Although the idea behind it is interesting, it doesn't seem worth the
    tradeoff of losing support for any other modules that are configured
    in terms of `networking.useNetworkd = true`.

--- hosts/tatl/networking.nix
+++ hosts/tatl/networking.nix
@@ -14,36 +14,15 @@
   environment.etc."resolv.conf".source =
     lib.mkForce  "/run/systemd/resolve/resolv.conf";
 
-  # NOTE: [systemd-networkd]
-  #
-  # This is a server with stable interfaces; in the interest of trying
-  # something new, its network will be configured as much through
-  # `systemd-networkd` as possible.
-  #
-  # In most cases this should be fine, and will provided added flexibility in
-  # terms of being able to set unit names and control settings.
-  #
-  # Some modules (e.g. `services.tailscale`) depend on `useNetworkd = true` to
-  # configure their own `systemd-networkd` units, so the tradeoff might not be
-  # worth it.
-  systemd.network = {
-    enable = true;
-    networks = {
-      "40-enp86s0" = {
-        matchConfig.Name = "enp86s0";
-        networkConfig = {
-          DHCP = "ipv4";
-          IPv6PrivacyExtensions = "kernel";
-        };
-      };
-    };
-  };
-
   networking = {
-    # NOTE: [systemd-networkd]
     dhcpcd.enable = false;
     useDHCP = false;
-    useNetworkd = false;
+
+    useNetworkd = true;
+    interfaces = {
+      enp86s0.useDHCP = true;
+      # wlo1.useDHCP = false;
+    };
 
     firewall = {
       enable = true;
