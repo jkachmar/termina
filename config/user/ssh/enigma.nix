749ec731effa9c14e3cc779a80b3d036661fb0c4
jkachmar <git@jkachmar.com>
2023-10-07 17:07:00 -0400

    misc: ssh config wrangling

--- config/user/ssh/enigma.nix
+++ config/user/ssh/enigma.nix
@@ -1,13 +1,14 @@
 {config, ...}: let
   sshDir = "${config.home.homeDirectory}/.ssh";
 in {
-  programs.ssh.matchBlocks."192.168.1.150" = {
-    hostname = "192.168.1.150";
-    user = config.home.username;
-    forwardAgent = true;
-    identityFile = [
-      "${sshDir}/id_gpg.pub"
-      "${sshDir}/id_enigma"
-    ];
+  programs.ssh.matchBlocks = {
+    "192.168.1.150".hostname = "192.168.1.150";
+    "enigma.thempire.dev".hostname = "enigma.thempire.dev";
+    "192.168.1.150 enigma.thempire.dev" = {
+      hostname = "192.168.1.150";
+      user = config.home.username;
+      forwardAgent = true;
+      identityFile = ["${sshDir}/id_gpg.pub"];
+    };
   };
 }
--- config/user/ssh/tatl.nix
+++ config/user/ssh/tatl.nix
@@ -1,10 +1,14 @@
 {config, ...}: let
   sshDir = "${config.home.homeDirectory}/.ssh";
 in {
-  programs.ssh.matchBlocks."192.168.1.151" = {
-    hostname = "192.168.1.151";
-    user = config.home.username;
-    forwardAgent = true;
-    identityFile = ["${sshDir}/id_gpg.pub"];
+  programs.ssh.matchBlocks = {
+    "tatl.thempire.dev".hostname = "tatl.thempire.dev";
+    "192.168.1.151".hostname = "192.168.1.151";
+    "tatl.thempire.dev 192.168.1.151" = {
+      hostname = "192.168.1.151";
+      user = config.home.username;
+      forwardAgent = true;
+      identityFile = ["${sshDir}/id_gpg.pub"];
+    };
   };
 }
