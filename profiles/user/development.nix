a95e503f8478714493dc75a224106f6290a91fc7
jkachmar <git@jkachmar.com>
2024-02-20 14:32:16 -0800

    user: enable git & jujutsu for dev work by default

--- profiles/user/development.nix
+++ profiles/user/development.nix
@@ -1,8 +1,10 @@
-{
+{lib, ...}: {
   imports = [
     ../../modules/user/git.nix
     ../../modules/user/jujutsu.nix
     ../../config/user/devtools.nix
     ../../config/user/neovim
   ];
+  programs.git.enable = lib.mkDefault true;
+  programs.jujutsu.enable = lib.mkDefault true;
 }
